ed9670c6e3827a1fff181d7e2c50a741583ec6b5
Ichoku Chinonso <greatchinonso7@gmail.com>
2021-01-11 02:06:41 +0100

    Setup and configuration

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Inner } from './styles/jumbotron';
+
+const Jumbotron = () => {
+  return (
+    <div>
+      <Inner>
+        <p>I am a Jumbotron</p>
+      </Inner>
+    </div>
+  )
+}
+
+export default Jumbotron;
